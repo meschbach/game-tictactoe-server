5344736dcf6400fe690c341cd7ee733d9011a9ba
Mark Eschbach <meschbach@gmail.com>
2018-08-03 19:24:33 -0700

    Adds broadcast

--- service.js
+++ service.js
@@ -1,10 +1,21 @@
 const ws = require("ws");
 
+const clients = [];
+
 const service = new ws.Server({port: process.env.PORT || 1234})
 service.on("connection", function( ws ){
+
+	clients.push(ws);
+
 	ws.on("message", function(m){
 		try {
-			console.log(m);
+			if( !m.type ){
+				clients.forEach( (c) => {
+					c.send(m);
+				});
+			} else {
+				console.log(m);
+			}
 		}catch(e){
 			console.error(e);
 		}
