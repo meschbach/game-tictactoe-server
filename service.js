0c51143da66873fc3358bec065ab32c34043f742
Mark Eschbach <meschbach@gmail.com>
2018-08-03 19:12:56 -0700

    NOw heroku compat

--- service.js
+++ service.js
@@ -1,6 +1,6 @@
 const ws = require("ws");
 
-const service = new ws.Server({port: 1234})
+const service = new ws.Server({port: process.env.PORT || 1234})
 service.on("connection", function( ws ){
 	ws.on("message", function(m){
 		try {
