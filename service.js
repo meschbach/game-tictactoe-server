3eb9b54a44bd8e895d0c7d893ab18effb8558a55
Mark Eschbach <meschbach@gmail.com>
2018-08-03 19:45:34 -0700

    Now as game~

--- service.js
+++ service.js
@@ -1,4 +1,34 @@
 const ws = require("ws");
+const EventEmitter = require("events");
+
+class Game extends EventEmitter {
+	constructor() {
+		super();
+
+		this.board = new Array(9);
+	}
+
+	move( user, x, y ){
+		if( this.lastMoved == user ){
+			return false;
+		}
+
+		const position = (y * 3) + x;
+		if( this.board[position] ){
+			return false;
+		}
+
+		this.lastmMoved = user;
+		this.board[position] = user;
+		this.emit("moved", {user, x, y});
+	}
+
+	boardState() {
+		return this.board;
+	}
+}
+
+const game = new Game();
 
 const clients = [];
 
@@ -6,18 +36,36 @@ const service = new ws.Server({port: process.env.PORT || 1234})
 service.on("connection", function( ws ){
 
 	clients.push(ws);
+	let user;
+
+	game.on("moved", function( e ) {
+		ws.send(JSON.stringify(Object.assign({type: "move"}, e)));
+	});
 
 	ws.on("message", function(m){
 		try {
-			if( !m.type ){
-				clients.forEach( (c) => {
-					c.send(m);
-				});
+			const frame = JSON.parse(m);
+			const type = frame.type;
+			if( !user ){
+				if( type != "user" ){
+					return ws.send( JSON.stringify({type:"error", error: "User name not declare"}));
+				}
+				user = frame.user;
 			} else {
-				console.log(m);
+				switch(type) {
+					case "move" :
+						game.move( user, frame.x, frame.y );
+						break;
+					case "state":
+						ws.send( JSON.stringify({type: "state", board: game.boardState() }));
+						break;
+					default:
+						return ws.send( JSON.stringify({type:"error", error: "Unknonw command", command: type}));
+				}
 			}
 		}catch(e){
 			console.error(e);
+			ws.send( JSON.stringify({type:"error", error: "Unable to parse frame", e}));
 		}
 	});
 });
